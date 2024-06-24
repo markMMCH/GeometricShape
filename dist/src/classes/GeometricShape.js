360e29b7dca0495c46211c1b836008117d5e7fb8
markMMCH <mark.chibukhchian@gmail.com>
2024-06-24 19:53:04 +0400

    removing files

--- dist/src/classes/GeometricShape.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GeometricShape = void 0;
-class GeometricShape {
-    constructor(name) {
-        this.name = name;
-    }
-}
-exports.GeometricShape = GeometricShape;
